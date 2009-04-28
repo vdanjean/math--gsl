672c22c9f0447b0bdd85ce327a2b84af89504414
Vincent Danjean <Vincent.Danjean@ens-lyon.org>
2009-04-28 04:03:25 +0200

    v2

--- helper/gsl-perl-helper.h
+++ helper/gsl-perl-helper.h
@@ -7,17 +7,67 @@
 
 #include <stdlib.h>
 
-#define perl_obj(pointer,ctype,perlclass) ({                 \
-   SV* ref=newSViv(0); SV* obj=newSVrv(ref, #perlclass);     \
-   sv_setiv(obj, (IV) (ctype*)pointer); SvREADONLY_on(obj);  \
-   ref;                                                      \
-})
+#define GSL_MAGIC_SIG 0xBEBE /* Uniq value */
 
-#define c_obj(sv,ctype,perlclass) (                      \
-   (sv_isobject(sv) && sv_derived_from(sv, #perlclass))  \
-      ? ((ctype*)SvIV(SvRV(sv)))                         \
-      : NULL                                             \
-   )
+static MAGIC* gsl_perl_mg_find (SV *sv, int type) {
+        if (sv) {
+                MAGIC *mg;
+                for (mg = SvMAGIC (sv); mg; mg = mg->mg_moremagic) {
+                        if (mg->mg_type == type && mg->mg_private == GSL_MAGIC_SIG)
+                                return mg;
+                }
+        }
+        return 0;
+}
+
+static SV * gsl_perl_bind_obj2perl(SV* sv, const char* perlclass, void* pointer) {
+	SV * tie;
+        HV * stash;
+        MAGIC * mg;
+
+        /* Create a tied reference. */
+        tie = newRV_noinc (sv);
+        stash = gv_stashpv (perlclass, GV_ADD);
+        sv_bless (tie, stash);
+        //sv_magic ((SV *) av, tie, PERL_MAGIC_tied, Nullch, 0);
+
+        /* Associate the array with the original path via magic. */
+        sv_magic (sv, 0, PERL_MAGIC_ext, (const char *) pointer, 0);
+
+	//SvREADONLY_on(sv);
+
+        mg = mg_find (sv, PERL_MAGIC_ext);
+
+        /* Mark the mg as belonging to us. */
+        mg->mg_private = GSL_MAGIC_SIG;
+
+#if PERL_REVISION <= 5 && PERL_VERSION <= 6
+        /* perl 5.6.x doesn't actually set mg_ptr when namlen == 0, so do it
+         * now. */
+        mg->mg_ptr = (char *) pointer;
+#endif /* 5.6.x */
+
+        return tie;
+}
+
+#define perl_obj(pointer,ctype,perlclass)				\
+	({								\
+		SV* ref=(SV*)newAV(); /*SV* obj=newSVrv(ref, #perlclass); \
+				   sv_setiv(obj, (IV) (ctype*)pointer);	\
+				   sv_setiv(obj, (IV) 42);*/		\
+		SV* sv=gsl_perl_bind_obj2perl(ref, #perlclass, (void*)pointer); \
+		sv;							\
+	})
+
+#define c_obj(_sv,ctype,perlclass)					\
+	({								\
+		MAGIC* mg;						\
+		SV* sv=(_sv);						\
+		( sv &&	SvROK (sv)					\
+		  && sv_isobject(sv) && sv_derived_from(sv, #perlclass) \
+		  && (mg = gsl_perl_mg_find (SvRV (sv), PERL_MAGIC_ext))) \
+			? ((ctype*)mg->mg_ptr) : NULL;			\
+	})
 
 typedef struct CObject {
 	SV* perlobj;
@@ -29,11 +79,25 @@ typedef struct MemArray {
 	SV* perlobj;
 	size_t datasize;
 	void* data;
+	int mortal;
 } MemArray;
 
 #define DEBUG_MEMARRAY(start, mem) \
-   fprintf(stderr, start "MemArray (data: %ld @ %p, perlobj %d @ %p) @ %p\n", \
-   mem->datasize, mem->data, mem->perlobj ? SvREFCNT(mem->perlobj) : 0, mem->perlobj, mem)
+	({	\
+		SV* ref=(mem->perlobj && (SvROK(mem->perlobj))) ? SvRV(mem->perlobj) : 0; \
+		fprintf(stderr, "*** " start				\
+			"MemArray[%i] (data: %ld @ %p, perlobj %d @ %p [%i] " \
+			"-> %d @ %p [%i]) @ %p\n",			\
+			mem->mortal,					\
+			mem->datasize, mem->data,			\
+			mem->perlobj ? SvREFCNT(mem->perlobj) : 0,	\
+			mem->perlobj,					\
+			mem->perlobj ? SvTYPE(mem->perlobj) : 0,	\
+			ref ? SvREFCNT(ref) : SvROK(mem->perlobj),	\
+			ref ? ref : 0,					\
+			ref ? SvTYPE(ref) : -1,				\
+			mem);						\
+	})
 
 void MemArray_DESTROY(MemArray* mem);
 
